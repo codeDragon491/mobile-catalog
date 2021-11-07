2e36cb8b53ab4c624f9147b0b116688f39834c01
Julia Trefas <JTR@Julias-MacBook-Pro.local>
2021-11-07 13:08:04 +0100

    built catalog page

---
+++ src/data/mockData.js
@@ -0,0 +1,226 @@
+export const mockData = [
+        {
+          "createdAt": "2021-11-05T21:38:38.516Z",
+          "name": "Apple iPhone 13",
+          "image": "https://www.telmore.dk/shop/images/8492919597042512/apple-iphone-13-mini-midnight-front.png",
+          "price": "239 DKK/mth",
+          "memory": "128 GB",
+          "id": "1"
+        },
+        {
+          "createdAt": "2021-11-05T19:01:25.340Z",
+          "name": "Apple iPhone 13",
+          "image": "https://www.telmore.dk/shop/images/8492919597042512/apple-iphone-13-mini-midnight-front.png",
+          "price": "277 DKK/mth",
+          "memory": "256 GB",
+          "id": "2"
+        },
+        {
+          "createdAt": "2021-11-05T20:01:31.074Z",
+          "name": "Apple iPhone 13",
+          "image": "https://www.telmore.dk/shop/images/8492919597042512/apple-iphone-13-mini-midnight-front.png",
+          "price": "354 DKK/mth",
+          "memory": "512 GB",
+          "id": "3"
+        },
+        {
+          "createdAt": "2021-11-05T14:51:15.593Z",
+          "name": "Apple iPhone 13 mini",
+          "image": "https://www.telmore.dk/shop/images/8492919597042512/apple-iphone-13-mini-midnight-front.png",
+          "price": "210 DKK/mth",
+          "memory": "128 GB",
+          "id": "4"
+        },
+        {
+          "createdAt": "2021-11-06T05:30:24.601Z",
+          "name": "Apple iPhone 13 mini",
+          "image": "https://www.telmore.dk/shop/images/8492919597042512/apple-iphone-13-mini-midnight-front.png",
+          "price": "249 DKK/mth",
+          "memory": "256 GB",
+          "id": "5"
+        },
+        {
+          "createdAt": "2021-11-05T12:42:41.244Z",
+          "name": "Apple iPhone 13 mini",
+          "image": "https://www.telmore.dk/shop/images/8492919597042512/apple-iphone-13-mini-midnight-front.png",
+          "price": "323 DKK/mth",
+          "memory": "512 GB",
+          "id": "6"
+        },
+        {
+          "createdAt": "2021-11-05T17:00:11.450Z",
+          "name": "Apple iPhone 13 Pro",
+          "image": "	https://www.telmore.dk/shop/images/8492919659002032/apple-iphone-13-pro-graphite-front.png",
+          "price": "315 DKK/mth",
+          "memory": "128 GB",
+          "id": "8"
+        },
+        {
+          "createdAt": "2021-11-06T04:14:22.435Z",
+          "name": "Apple iPhone 13 Pro",
+          "image": "https://www.telmore.dk/shop/images/8492919659002032/apple-iphone-13-pro-graphite-front.png",
+          "price": "359 DKK/mth",
+          "memory": "256 GB",
+          "id": "9"
+        },
+        {
+          "createdAt": "2021-11-06T07:41:56.599Z",
+          "name": "Apple iPhone 13 Pro",
+          "image": "https://www.telmore.dk/shop/images/8492919659002032/apple-iphone-13-pro-graphite-front.png",
+          "price": "425 DKK/mth",
+          "memory": "512 GB",
+          "id": "10"
+        },
+        {
+          "createdAt": "2021-11-05T14:56:25.315Z",
+          "name": "Apple iPhone 13 Pro",
+          "image": "https://www.telmore.dk/shop/images/8492919659002032/apple-iphone-13-pro-graphite-front.png",
+          "price": "499 DKK/mth",
+          "memory": "1 TB",
+          "id": "11"
+        },
+        {
+          "createdAt": "2021-11-06T02:36:07.735Z",
+          "name": "Apple iPhone 13 Pro Max",
+          "image": "https://www.telmore.dk/shop/images/8492919659002032/apple-iphone-13-pro-graphite-front.png",
+          "price": "345 DKK/mth",
+          "memory": "128 GB",
+          "id": "12"
+        },
+        {
+          "createdAt": "2021-11-06T09:24:20.830Z",
+          "name": "Apple iPhone 13 Pro Max",
+          "image": "https://www.telmore.dk/shop/images/8492919659002032/apple-iphone-13-pro-graphite-front.png",
+          "price": "385 DKK/mth",
+          "memory": "256 GB",
+          "id": "13"
+        },
+        {
+          "createdAt": "2021-11-06T01:09:52.810Z",
+          "name": "Apple iPhone 13 Pro Max",
+          "image": "https://www.telmore.dk/shop/images/8492919659002032/apple-iphone-13-pro-graphite-front.png",
+          "price": "455 DKK/mth",
+          "memory": "512 GB",
+          "id": "14"
+        },
+        {
+          "createdAt": "2021-11-05T16:40:08.413Z",
+          "name": "Apple iPhone 13 Pro Max",
+          "image": "https://www.telmore.dk/shop/images/8492919659002032/apple-iphone-13-pro-graphite-front.png",
+          "price": "525 DKK/mth",
+          "memory": "1 TB",
+          "id": "15"
+        },
+        {
+          "createdAt": "2021-11-05T20:36:20.451Z",
+          "name": "Apple iPhone SE 2020",
+          "image": "https://www.telmore.dk/shop/images/8474621197106064/apple-iphone-se-2020-black-front.png",
+          "price": "2.599 DKK",
+          "memory": "64 GB",
+          "id": "16"
+        },
+        {
+          "createdAt": "2021-11-05T21:45:04.989Z",
+          "name": "Apple iPhone SE 2020",
+          "image": "https://www.telmore.dk/shop/images/8474621197106064/apple-iphone-se-2020-black-front.png",
+          "price": "3.099 DKK",
+          "memory": "128 GB",
+          "id": "17"
+        },
+        {
+          "createdAt": "2021-11-05T10:29:56.847Z",
+          "name": "Apple iPhone SE 2020",
+          "image": "https://www.telmore.dk/shop/images/8474621197106064/apple-iphone-se-2020-black-front.png",
+          "price": "3.999 DKK",
+          "memory": "256 GB",
+          "id": "18"
+        },
+        {
+          "createdAt": "2021-11-06T04:08:58.385Z",
+          "name": "Samsung Galaxy S21 5G",
+          "image": "https://www.telmore.dk/shop/images/8457393796356816/samsung-galaxy-s21-phantom-gray-front.png",
+          "price": "219 DKK/mth",
+          "memory": "128 GB",
+          "id": "24"
+        },
+        {
+          "createdAt": "2021-11-05T15:03:03.156Z",
+          "name": "Samsung Galaxy S21 5G",
+          "image": "https://www.telmore.dk/shop/images/8457393796356816/samsung-galaxy-s21-phantom-gray-front.png",
+          "price": "249 DKK/mth",
+          "memory": "256 GB",
+          "id": "25"
+        },
+        {
+          "createdAt": "2021-11-05T10:12:56.453Z",
+          "name": "Samsung Galaxy S21+ 5G",
+          "image": "	https://www.telmore.dk/shop/images/8457393796356816/samsung-galaxy-s21-phantom-gray-front.png",
+          "price": "289 DKK/mth",
+          "memory": "128 GB",
+          "id": "26"
+        },
+        {
+          "createdAt": "2021-11-05T18:45:44.268Z",
+          "name": "Samsung Galaxy S21+ 5G",
+          "image": "https://www.telmore.dk/shop/images/8457393796356816/samsung-galaxy-s21-phantom-gray-front.png",
+          "price": "309 DKK/mth",
+          "memory": "256 GB",
+          "id": "27"
+        },
+        {
+          "createdAt": "2021-11-05T14:14:31.786Z",
+          "name": "Samsung Galaxy S21 Ultra 5G",
+          "image": "https://www.telmore.dk/shop/images/8457395177227504/samsung-galaxy-s21-ultra-phantom-black-front.png",
+          "price": "339 DKK/mth",
+          "memory": "",
+          "id": "31"
+        },
+        {
+          "createdAt": "2021-11-05T19:32:11.309Z",
+          "name": "Samsung Galaxy S21 Ultra 5G",
+          "image": "https://www.telmore.dk/shop/images/8457395177227504/samsung-galaxy-s21-ultra-phantom-black-front.png",
+          "price": "359 DKK/mth",
+          "memory": "",
+          "id": "32"
+        },
+        {
+          "createdAt": "2021-11-06T07:16:53.075Z",
+          "name": "Samsung Galaxy S21 Ultra 5G",
+          "image": "https://www.telmore.dk/shop/images/8457395177227504/samsung-galaxy-s21-ultra-phantom-black-front.png",
+          "price": "399 DKK/mth",
+          "memory": "",
+          "id": "33"
+        },
+        {
+          "createdAt": "2021-11-05T13:47:06.220Z",
+          "name": "Samsung Galaxy A52s 5G",
+          "image": "	https://www.telmore.dk/shop/images/8492896126279760/samsung-galaxy-a52s-mint-front.png",
+          "price": "2.499 DKK",
+          "memory": "128 GB",
+          "id": "38"
+        },
+        {
+          "createdAt": "2021-11-05T11:24:57.624Z",
+          "name": "Samsung Galaxy A52 5G",
+          "image": "https://www.telmore.dk/shop/images/8457514247055568/samsung-galaxy-a52-5g-white-front.png",
+          "price": "2.999 DKK",
+          "memory": "128 GB",
+          "id": "39"
+        },
+        {
+          "createdAt": "2021-11-05T12:55:21.243Z",
+          "name": "Samsung Galaxy A32 5G",
+          "image": "https://www.telmore.dk/shop/images/8457444855343792/samsung-galaxy-a32-5g-sort-front.png",
+          "price": "1.999 DKK",
+          "memory": "64 GB",
+          "id": "40"
+        },
+        {
+          "createdAt": "2021-11-05T19:31:39.304Z",
+          "name": "Samsung A22 5G",
+          "image": "https://www.telmore.dk/shop/images/8492879663141152/samsung-galaxy-a22-5g-grey-front.png",
+          "price": "999 DKK",
+          "memory": "64 GB",
+          "id": "41"
+        }
+]
--- src/views/mobiles/Index.vue
+++ src/views/mobiles/Index.vue
@@ -1,19 +1,93 @@
 <template>
-  <div id="catalog">
+  <div id="catalog-page">
     <h1>Mobilephones</h1>
     <p>
       Here you can see our large selection of mobile phones.<br>
       You always get a screen change when you buy your mobile with a subscription.
     </p>
+   <section class="mobiles-section">
+     <div class="mobile-card"
+        v-for="mobile in mobiles"
+        :key="mobile.id"
+      >
+      <p class="title">{{ mobile.name }}</p>
+      <p class="memory">{{ mobile.memory }}</p>
+      <div class="image" 
+        :style="{ backgroundImage: `url(${mobile.image})` }"
+      />
+      <p class="price">{{ mobile.price }}</p>
+     </div>
+   </section>
   </div>
 </template>
 
 <script>
+import {mockData} from '@/data/mockData.js'
 export default {
   name: 'Catalog',
+  data () {
+    return {
+      mobiles: mockData
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#catalog-page {
+  margin: 0 2rem;
+}
+.mobiles-section {
+  margin: 2rem 0;
+  display: grid;
+  grid-column-gap: 2rem;
+  grid-row-gap: 2rem;
+  grid-template-columns: 100%;
+  justify-content: center;
+}
+.mobile-card {
+  cursor: pointer;
+  background-color: var(--white);
+  border-radius: 1.25rem;
+  transition: all .3s linear;
+  border: 1px solid var(--white);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.mobile-card:hover {
+  border: 1px solid var(--light-grey);
+}
+.image {
+  height: 285px;
+  width: 180px;
+  background-position: center;
+  background-repeat: no-repeat; 
+  background-size: 75% 100%;
+}
+.title {
+  margin: 1.875rem 0 0.5rem;
+}
+.memory {
+  font-size: 0.75rem;
+  margin: 0 0 1rem;
+}
+.price {
+  margin-top: 1rem;
+  font-size: 1.25rem;
+}
+@media screen and (min-width: 768px) {
+  .mobiles-section {
+    grid-template-columns: repeat(auto-fill, 240px);
+  }
+}
+@media screen and (min-width: 1024px) {
+  #catalog-page {
+    margin: 0 15rem;
+  }
+  .mobiles-section {
+    margin: 5rem 0;
+  }
+}
 </style>
